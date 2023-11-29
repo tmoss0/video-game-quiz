cd1d166720f00eb706d03272ddcd3c4aaed1806a
Tim Moss <tmoss726@hotmail.com>
2023-11-29 14:27:43 -0500

    Remove score to deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
 import './App.css';
 import Navigation from './components/Navigation';
 import { Outlet } from 'react-router-dom';
 
 const App: React.FC = () => {
-  const [score, setScore] = useState(0);
 
   return (
     <>
